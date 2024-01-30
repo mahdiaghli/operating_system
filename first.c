588266a5a6813eb009a6bb227975fd9c94967ded
Aminmdge <m.amin.h.2646@gmail.com>
2024-01-30 18:24:02 +0330

    get path and pattern wirh args from cmd

--- first.c
+++ first.c
@@ -17,7 +17,7 @@
 
 #define MAX_FILES 100
 #define MAX_LINE_LENGTH 256
-//
+
 // Structure to hold file information and search pattern
 typedef struct {
     char* files[MAX_FILES];
@@ -27,6 +27,7 @@ typedef struct {
     sem_t sem;
 } ProcessData;
 
+// Structure to hold each thread information witch is passed to the process_file function
 typedef  struct {
     int* matchedFile;
     ProcessData* data;
@@ -34,21 +35,21 @@ typedef  struct {
     FILE* file;
 } ThreadResult;
 
-//ProcessData* data, FILE* file, int* matched_files, const int* i
 void *process_file(ThreadResult *args) {
 
     char line[MAX_LINE_LENGTH];
+    char *charnum;
     int line_number = 0;
 
     while (fgets(line, sizeof(line), args->file) != NULL) {
         line_number++;
 
         // Check if the line contains the specified pattern
-        if (strstr(line, args->data->pattern) != NULL) {
+        if ((charnum = strstr(line, args->data->pattern)) != NULL) {
             // Use lock for synchronized printing
             pthread_mutex_lock(&args->data->mutex);
             sem_wait(&args->data->sem);
-            printf("%s:%d: %s\n", args->data->files[args->i], line_number, line);
+            printf("%s:%d:%d: %s", args->data->files[args->i], line_number, (int)(charnum - line) + 1, line);
             (*args->matchedFile)++;
             sem_post(&args->data->sem);
             pthread_mutex_unlock(&args->data->mutex);
@@ -68,8 +69,11 @@ void process_files_with_thread(ProcessData* data,  int* checked_files, int* matc
             perror("Error opening file");
             continue;
         }
-
+        pthread_mutex_lock(&data->mutex);
+        sem_wait(&data->sem);
         (*checked_files)++;
+        sem_post(&data->sem);
+        pthread_mutex_unlock(&data->mutex);
 
         thread_results[i].i = i;
         thread_results[i].matchedFile = matched_files;
@@ -83,33 +87,6 @@ void process_files_with_thread(ProcessData* data,  int* checked_files, int* matc
     }
 
 
-}
-
-
-// Function to process files (lines containing the specified pattern)
-void process_files_with_thread(ProcessData* data,  int* checked_files, int* matched_files) {
-    pthread_t process_file_thread[data->file_count];
-    ThreadResult thread_results[data->file_count];
-
-    for (int i = 0; i < data->file_count; ++i) {
-        FILE* file = fopen(data->files[i], "r");
-        if (file == NULL) {
-            perror("Error opening file");
-            continue;
-        }
-
-        (*checked_files)++;
-
-        thread_results[i].i = i;
-        thread_results[i].matchedFile = matched_files;
-        thread_results[i].data = data;
-        thread_results[i].file = file;
-
-        pthread_create(&process_file_thread[i], NULL, process_file, &thread_results[i]);
-    }
-    for (int i = 0; i < data->file_count; ++i) {
-        pthread_join(process_file_thread[i], NULL);
-    }
 }
 
 // Recursive function to traverse directories
@@ -145,10 +122,9 @@ void traverse_directory(const char* path, ProcessData* data, int* checked_files,
     closedir(dir);
 }
 
-
-int main() {
-    const char* root_directory = "/Users/amin/Documents/دانشگاه/۵.سیستم عامل/grep/testfiles";
-    const char* search_pattern = "hello";  // Replace with your desired pattern
+int main(int argc, char* argv[]) {
+    const char* root_directory = argv[1];
+    const char* search_pattern = argv[2];  // Replace with your desired pattern
 
     ProcessData main_data;
 
@@ -160,12 +136,6 @@ int main() {
 
     *checked_files = 0;
     *matched_files = 0;
-//    (*checked_files)++;
-
-
-//    Result result;
-//    result.checkedFiles = 0;
-//    result.matchedFile = 0;
 
     main_data.file_count = 0;
     main_data.pattern = (char*)search_pattern;
@@ -184,7 +154,6 @@ int main() {
     pthread_mutex_destroy(&main_data.mutex);  // Destroy mutex
     sem_destroy(&main_data.sem);  // Destroy semaphore
 
-    printf("Total number of files: %d\nTotal number of Matches: %d", *checked_files, *matched_files);
-
+    printf("Total number of files: %d\nTotal number of Matches: %d\n", *checked_files, *matched_files);
     return 0;
 }
